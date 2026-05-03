65c3a5ac64aee6d33db0ef439fedff37ee561652
yangyue-l <1695847539@qq.com>
2026-05-03 09:09:46 +0800

    小程序登录功能并添加拦截器

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.sky.entity.User;
+
+@Mapper
+public interface UserMapper {
+
+
+    @Select("select * from user where openid = #{openId}")
+    User getByUserOpenid(String opeanid);
+
+    
+    void insert(User user);
+    
+}
